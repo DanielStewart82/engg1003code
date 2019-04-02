2f3ec6a4e91ccf5a2dc5df287637c36d253b0cad
Daniel Stewart <sailordan.coyote@gmail.com>
2019-04-02 04:51:05 +0000

    master: ASCII loop

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main() {
-	printf("Woo! You got the template working!\n");
+/*int main() {
+	char string[4]="test";
+	int k;
+	k=5;
+	printf("%s\n", string);
+    printf("%d\n", string[2]+k);
+	
+	return 0;
+}*/
+int main(){
+
+char string[]="test";
+    int n, i;
+    n = (sizeof(string)) / (sizeof(string[0]));
+	int k;
+	k=0;
+	printf("%s\n", string);
+	for(i=0;i<=n; i++){
+	    i = string[i];
+    printf("%d\n", string[i]+k);
+}
+
 	return 0;
 }
